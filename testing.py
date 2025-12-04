4cf80eb8d755a70900d104cba017e12c6494fa7e
Aayansh Niranjan <aayansh.singh007i@gmail.com>
2025-12-05 03:23:10 +0530

    Folder Structure changes

--- testing.py
+++
@@ -1,7 +0,0 @@
-from Backend.Extracter import TextExtracter
-
-Extracter = TextExtracter()
-
-output = Extracter.handleFiles(r"C:/Users/shrey/Downloads/yellow_ranger_dummy_document.pdf")
-
-print(output["summary"])
